759c3742681833dc2e81c0cb7d3e00e1a06eafcc
RomeroBarata <romero.barata@gmail.com>
2017-07-06 16:36:50 +1000

    Implement the simple measures 'num_examples' and 'num_features'

---
+++ R/simple-measures.R
@@ -0,0 +1,25 @@
+#' The Number of Observations in the Data Set.
+#'
+#' \code{num_examples} computes the number of examples present in the input
+#' data set. It is just a wrapper for the \code{nrow} function.
+#'
+#' @inheritParams F1
+#' @return The number of examples present in the input data set.
+#' @export
+
+num_examples <- function(x){
+  nrow(x)
+}
+
+#' The Number of Features in the Data Set.
+#'
+#' \code{num_features} computes the number of features present in the input
+#' data set. It is just a wrapper for the \code{ncol} function.
+#'
+#' @inheritParams F1
+#' @return The number of features present in the input data set.
+#' @export
+
+num_features <- function(x){
+  ncol(x)
+}
