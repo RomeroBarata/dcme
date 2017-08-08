7f04f314421c7f6ec65ee8f1f1ae8a3d7499a38a
RomeroBarata <romero.barata@gmail.com>
2017-08-08 15:37:03 +1000

    Implement function to split a data set into predictors and target

---
+++ R/utils.R
@@ -0,0 +1,26 @@
+#' Split a Data Set into Predictors and Target.
+#'
+#' \code{split_x_and_y} splits the input data set into the predictors x
+#' and the target y.
+#'
+#' @param data A data frame containing both predictors and target variables.
+#' @param target The column index or the name of the target variable. If not
+#'  provided the target is assumed to be the last column of \code{data}.
+#' @return A list containing the predictors (x) and the target (y) variables.
+#' @export
+
+split_x_and_y <- function(data, target = ncol(data)){
+  y <- data[[target]]
+
+  if (is.numeric(target)){
+    x <- data[-target]
+  } else if (is.character(target)){
+    target <- which(colnames(data) == target)
+    x <- data[-target]
+  } else{
+    stop("target must be either the target column index or name.",
+         call. = FALSE)
+  }
+
+  list(x = x, y = y)
+}
